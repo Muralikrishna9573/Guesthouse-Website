4bcb84973585c0a6b68560092c9d0492f69f011f
Muralikrishna9573 <sunnygodavarthi7@gmail.com>
2024-12-27 23:01:55 +0530

    gallery

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import small from '../assets/kingsukh guesh house assets/small.jpg';
+import room1 from '../assets/kingsukh guesh house assets/room1.jpg';
+import recep from '../assets/kingsukh guesh house assets/recep.jpg';
+
 
 const Gallery = () => {
   return (
-    <div className='bg-violet-400 m-8 rounded-xl'>
+    <div className='bg-white-400 border-2 m-12 rounded-xl'>
       <section class="text-gray-600 body-font">
-  <div class="container px-8  py-22 mx-auto">
-    <div class="flex flex-wrap -m-4">
+  <div class="container px-24 mx-auto">
+    <div class="flex flex-wrap border-2  m-">
       <div class="p-4 md:w-1/3">
-        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+        <div class="h-full border-2 border-gray-200 border-opacity-60  rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={small} alt="blog"/>
           <div class="p-6">
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
@@ -37,7 +41,7 @@ const Gallery = () => {
       </div>
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog"/>
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={room1} alt="blog"/>
           <div class="p-6">
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
@@ -66,7 +70,7 @@ const Gallery = () => {
       </div>
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"/>
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={recep} alt="blog"/>
           <div class="p-6">
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
